745c3da33a109e2d2c0723a0e41103f837f3ec53
dev-nomi <noman.khalid.freelance@gmail.com>
2022-07-07 02:20:42 +0500

    Added more universities in seed file

--- init_db.py
+++ init_db.py
@@ -40,23 +40,64 @@ university5 = University(
 )
 
 university6 = University(
-  name="University  of the punjab", 
+  name="University of the punjab", 
   course_offered="BS Computer Sciences", 
   fee=48650
 )
 
 university7 = University(
-  name="University  of the punjab", 
+  name="University of the punjab", 
   course_offered="BS Software Engineering", 
   fee=48650
 )
 
 university8 = University(
-  name="University  of the punjab", 
+  name="University of the punjab", 
   course_offered="BS Chemical Engineering", 
   fee=27650
 )
 
+university9 = University(
+  name="University of the punjab", 
+  course_offered="BS Electrical Engineering", 
+  fee=22700
+)
+
+university10 = University(
+  name="University of Central Punjab", 
+  course_offered="BS Computer Sciences", 
+  fee=202625
+)
+
+university11 = University(
+  name="University of Central Punjab", 
+  course_offered="BS Software Engineering", 
+  fee=186000
+)
+
+university12 = University(
+  name="University of Central Punjab", 
+  course_offered="BS Electrical Engineering", 
+  fee=176500
+)
+
+university13 = University(
+  name="Information Technology University", 
+  course_offered="BS Electrical Engineering", 
+  fee=114000
+)
+
+university14 = University(
+  name="Information Technology University", 
+  course_offered="BS Computer Sciences", 
+  fee=114000
+)
+
+university15 = University(
+  name="Information Technology University", 
+  course_offered="BS Computer Engineering", 
+  fee=114000
+)
 
 db.session.add_all([user1,user2])
 db.session.add_all([feedback1, feedback2])
@@ -67,7 +108,15 @@ db.session.add_all([
   university4, 
   university5, 
   university6,
-  university7
+  university7,
+  university8,
+  university9,
+  university10,
+  university11,
+  university12,
+  university13,
+  university14,
+  university15
 ])
 
 
